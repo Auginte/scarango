ae414e0ae27233535bf0b39c8445c1b40d74b819
Aurelijus Banelis <aurelijus@banelis.lt>
2015-08-23 16:57:11 +0300

    Java 1.6 backward compatibility: Date format

--- src/main/scala/com/auginte/scarango/common/TestKit.scala
+++ src/main/scala/com/auginte/scarango/common/TestKit.scala
@@ -9,7 +9,7 @@ import scala.util.Random
  * Small function for easier testing of the driver
  */
 object TestKit {
-  def date = new SimpleDateFormat("YYYY-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime)
+  def date = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime)
 
   def unique = "test_" + date + "_" + Random.nextInt(999)
 }
